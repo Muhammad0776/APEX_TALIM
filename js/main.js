1113b683a7652726dc0a22a2625d66600085b78d
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-10-05 18:31:59 +0500

    carousel

--- js/main.js
+++ js/main.js
@@ -11,4 +11,26 @@ $(document).ready(function () {
       $(".scroll-up-btn").removeClass("show");
     }
   });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 0,
+        nav: false,
+      },
+      500: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 2,
+        nav: false,
+      },
+    },
+  });
 });
